ae5e3b70c8fbbacd6429ab06b4c7d87dae3cd2ad
Raul5304 <raullecinena@iesrioarba.es>
2026-01-29 19:51:45 +0100

    Creación de las validaciones específicas

--- script.js
+++ script.js
@@ -51,3 +51,72 @@ function validarCampo(campo, validaciones, mensajeError) {
   }
   mostrarExito(campo);
 }
+
+// VALIDACIONES ESPECÍFICAS
+function validarNombre() {
+  validarCampo(
+    campos.nombre,
+    validaciones.nombre,
+    "El nombre debe tener al menos 3 letras y solo contener letras y espacios"
+  );
+}
+
+function validarEmail() {
+  validarCampo(
+    campos.email,
+    validaciones.email,
+    "Introduce un correo electrónico válido"
+  );
+}
+
+function validarTelefono() {
+  validarCampo(
+    campos.telefono,
+    validaciones.telefono,
+    "El teléfono debe ser un móvil español válido (9 dígitos)"
+  );
+}
+
+function validarDNI() {
+  validarCampo(
+    campos.dni,
+    validaciones.dni,
+    "El DNI debe tener 8 números y una letra mayúscula"
+  );
+}
+
+function validarPassword() {
+  validarCampo(
+    campos.password,
+    validaciones.password,
+    "La contraseña debe tener mínimo 8 caracteres, una mayúscula y un número"
+  );
+}
+
+function validarPasswordConfirm() {
+  if (campos.password.value !== campos.passwordConfirm.value) {
+    throw new Error("Las contraseñas no coinciden");
+  }
+  mostrarExito(campos.passwordConfirm);
+}
+
+function validarEdad() {
+  const fecha = new Date(campos.birthdate.value);
+  if (!campos.birthdate.value) {
+    throw new Error("Debes introducir tu fecha de nacimiento");
+  }
+
+  // Para calcular correctamente la edad dependiendo si ha cumplido años o no
+  const hoy = new Date();
+  let edad = hoy.getFullYear() - fecha.getFullYear();
+  const mes = hoy.getMonth() - fecha.getMonth();
+  if (mes < 0 || (mes === 0 && hoy.getDate() < fecha.getDate())) {
+    edad--;
+  }
+
+  if (edad < 18) {
+    throw new Error("Debes ser mayor de 18 años para registrarte");
+  }
+
+  mostrarExito(campos.birthdate);
+}
