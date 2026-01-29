944a51823915f5ccdb231b434c88c23978d7a9b8
Raul5304 <raullecinena@iesrioarba.es>
2026-01-29 15:41:05 +0100

    Añadidas funciones de la interfaz

--- script.js
+++ script.js
@@ -20,3 +20,34 @@ const validaciones = {
   dni: /^\d{8}[A-Z]$/,
   password: /^(?=.*[A-Z])(?=.*\d).{8,}$/
 };
+
+// FUNCIONES UI
+function mostrarError(campo, mensaje) {
+  campo.classList.remove("valido");
+  campo.classList.add("invalido");
+
+  let error = campo.nextElementSibling;
+  if (!error || !error.classList.contains("error-msg")) {
+    error = document.createElement("div");
+    error.classList.add("error-msg");
+    campo.insertAdjacentElement("afterend", error);
+  }
+  error.textContent = mensaje;
+}
+
+function mostrarExito(campo) {
+  campo.classList.remove("invalido");
+  campo.classList.add("valido");
+
+  const error = campo.nextElementSibling;
+  if (error && error.classList.contains("error-msg")) {
+    error.remove();
+  }
+}
+
+function validarCampo(campo, regex, mensajeError) {
+  if (!regex.test(campo.value.trim())) {
+    throw new Error(mensajeError);
+  }
+  mostrarExito(campo);
+}
